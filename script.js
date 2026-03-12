18027858e391a45c25c10f8d3ee1dd66fd39f02e
Harry Hendry <harryhendry89@hotmail.co.uk>
2026-03-12 11:32:47 +0000

    we start again, official

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-const hamburger = document.querySelector('.hamburger');
-hamburger.onclick = function() {
+// JS NAVBAR
+const Hamburger = document.querySelector('.hamburger');
+Hamburger.addEventListener('click', () => {
     const Navbar = document.querySelector('.navbar');
-    Navbar.classList.toggle('active')
-}
+    Navbar.classList.toggle('active');
+}) 
+
+
+
+
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e){
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
